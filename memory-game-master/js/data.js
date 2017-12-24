c81e17ff154614affc12e6554d9722bcd0f833d0
richard <richlip@googlemail.com>
2017-12-24 10:36:59 +0100

    1st

---
+++ memory-game-master/js/data.js
@@ -0,0 +1,82 @@
+const cardData = [{
+    name: "Pikachu",
+    image: "pikachu.png",
+    id: "pikachu"
+  },
+  {
+    name: "Bulbasaur",
+    image: "bulbasaur.png",
+    id: "bulbasaur"
+  },
+  {
+    name: "Charmander",
+    image: "charmander.png",
+    id: "charmander"
+  },
+  {
+    name: "Master Ball",
+    image: "masterball.png",
+    id: "masterball"
+  },
+  {
+    name: "Great Ball",
+    image: "greatball.png",
+    id: "greatball"
+  },
+  {
+    name: "Poke Ball",
+    image: "pokeball.png",
+    id: "pokeball"
+  },
+  {
+    name: "Premier Ball",
+    image: "premierball.png",
+    id: "premierball"
+  },
+  {
+    name: "Meowth",
+    image: "meowth.png",
+    id: "meowth"
+  },
+  {
+    name: "Snorlax",
+    image: "snorlax.png",
+    id: "snorlax"
+  },
+  {
+    name: "Wigglypuff",
+    image: "wigglypuff.png",
+    id: "wigglypuff"
+  },
+  {
+    name: "Squirtle",
+    image: "squirtle.png",
+    id: "squirtle"
+  },
+  {
+    name: "Egg",
+    image: "egg.png",
+    id: "egg"
+  }
+];
+
+const gameLevels = {
+  easy: {
+    class: "easy",
+    pairs: 6,
+    twoStar: 6,
+    oneStar: 10
+  },
+  medium: {
+    class: "medium",
+    pairs: 8,
+    twoStar: 10,
+    oneStar: 14
+  },
+  hard: {
+    class: "hard",
+    pairs: 12,
+    twoStar: 16,
+    oneStar: 24
+  }
+};
---
+++ udacity-fend-matching-game-master/README.md
@@ -0,0 +1,22 @@
+# Udacity - Memory Game
+by Millie Lin
+
+## How the game was created
+The memory game is created base on the starter kit: [Udacity memory game repository](https://github.com/udacity/fend-project-memory-game) and written in HTML, CSS and JavaScript.
+
+The timer is from the EasyTimer.js (http://albert-gonzalez.github.io/easytimer.js/) by Albert González.
+
+The animation is from [animista.net](http:/animista.net) by ANA TRAVAS.
+
+## How to play the game
+Click a card to open it, if two opened cards matches, they will keep opening. 
+If two opened cards do not match, they will flip and you need to remember the symbol and explore the matched one. 
+
+When all cards matches, you win. 
+
+## What you can gain
+Try to win the game with less moves and you will be awarded with more stars. 
+Keep training your memory. 
+
+Good luck.   
+
